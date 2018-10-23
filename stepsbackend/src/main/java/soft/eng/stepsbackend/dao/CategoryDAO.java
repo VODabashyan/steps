443797580f5b47c8b33fe8ba2994c05671702392
Vahe Odabashyan <vaheodabashyan@gmail.com>
2018-10-24 00:03:18 +0400

    creating data transfer object (DTO) and data access object (DAO) classes

---
+++ stepsbackend/src/main/java/soft/eng/stepsbackend/dao/CategoryDAO.java
@@ -0,0 +1,12 @@
+package soft.eng.stepsbackend.dao;
+
+import java.util.List;
+
+import soft.eng.stepsbackend.dto.Category;
+
+public interface CategoryDAO {
+	
+	
+	List<Category> list(); //list of categories
+
+}
